4a22aae2431f9457e0c397db6022c6d2b0e7d773
AKASHT27 <93002388+AKASHT27@users.noreply.github.com>
2022-01-08 23:51:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>prototype</title>
+     <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <h1> prototype to check prime no is present in array or not</h1>
+    <input type="text" id="inpfield" ><br>
+    <button id="submit" onclick="fn()">Submit</button>
+    <div id="results"></div>
+    <script src="script.js"></script>
+</body>
+</html>
